13aacbce5066d3c3f5b055e719562ec7447d7cba
Taji Abdullah <tajiabdullah1@gmail.com>
2024-04-24 21:51:51 -0400

    Added functions.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,62 @@
 use slint::SharedString;
+
 slint::include_modules!();
 
-const TAX_PERCENTAGE: f64 = 0.30;
-const OWNER_PERCENTAGE: f64 = 0.55;
-const PROFIT_PERCENTAGE: f64 = 0.05;
-const OPERATION_EXPENSE_PERCENTAGE: f64 = 0.10;
+// Struct for percentage division
+struct IncomePercentage {
+    tax: f64,
+    owner: f64,
+    profit: f64,
+    operation_expense: f64,
+}
 
-
+// Main function, application entry point.
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     ui.on_divide_income({
         let ui_handle = ui.as_weak();
-        move |string: SharedString| {
+        move |user_input: SharedString| {
             let ui = ui_handle.unwrap();
-            let user_input: f64 = string.trim().parse().unwrap();
-            let tax: f64 = user_input * TAX_PERCENTAGE;
-            let owner: f64 = user_input * OWNER_PERCENTAGE;
-            let profit: f64 = user_input * PROFIT_PERCENTAGE;
-            let operating_expense: f64 = user_input * OPERATION_EXPENSE_PERCENTAGE;
-            let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOperating Expense:\
-                {:.2}\n", tax, owner, profit, operating_expense);
+            let user_input: f64 = user_input.trim().parse().unwrap();
 
-            ui.set_results(result.into());
+            let income_percentage = IncomePercentage {
+                tax: calculate_tax_percentage(user_input),
+                owner: calculate_owner_percentage(user_input),
+                profit: calculate_profit_percentage(user_input),
+                operation_expense: calculate_operation_expense(user_input),
+            };
+
+            let formated_calculation_result_string =
+                format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOperating Expense: {:.2}\n",
+                        income_percentage.tax, income_percentage.owner, income_percentage.profit,
+                        income_percentage.operation_expense);
+
+            ui.set_results(formated_calculation_result_string.into());
         }
     });
 
     ui.run()
 }
+
+fn calculate_tax_percentage(input: f64) -> f64 {
+    const TAX_PERCENTAGE: f64 = 0.30;
+    input * TAX_PERCENTAGE
+}
+
+fn calculate_owner_percentage(input: f64) -> f64 {
+    const OWNER_PERCENTAGE: f64 = 0.55;
+    input * OWNER_PERCENTAGE
+}
+
+fn calculate_profit_percentage(input: f64) -> f64 {
+    const PROFIT_PERCENTAGE: f64 = 0.05;
+    input * PROFIT_PERCENTAGE
+}
+
+fn calculate_operation_expense(input: f64) -> f64 {
+    const OPERATION_EXPENSE_PERCENTAGE: f64 = 0.10;
+    input * OPERATION_EXPENSE_PERCENTAGE
+}
+
+
