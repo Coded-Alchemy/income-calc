20983d3907080595436caf86cd581e961c06597d
Taji Abdullah <tajiabdullah1@gmail.com>
2024-04-28 20:24:09 -0400

    Prevent app crash from no input.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 mod income_calc;
+mod income_percentage;
+mod use_case;
 
 use slint::SharedString;
-use income_calc::calculations;
+use income_calc::use_case;
+use income_percentage::percent::IncomePercentage;
 
 slint::include_modules!();
-
-// Struct for percentage division
-struct IncomePercentage {
-    tax: f64,
-    owner: f64,
-    profit: f64,
-    operation_expense: f64,
-}
 
 // Main function, application entry point.
 fn main() -> Result<(), slint::PlatformError> {
@@ -21,21 +16,24 @@ fn main() -> Result<(), slint::PlatformError> {
         let ui_handle = ui.as_weak();
         move |user_input: SharedString| {
             let ui = ui_handle.unwrap();
-            let user_input: f64 = user_input.trim().parse().unwrap();
 
-            let income_percentage = IncomePercentage {
-                tax: calculations::calculate_tax_percentage(user_input),
-                owner: calculations::calculate_owner_percentage(user_input),
-                profit: calculations::calculate_profit_percentage(user_input),
-                operation_expense: calculations::calculate_operation_expense(user_input),
-            };
+            if (!user_input.is_empty()) {
+                let user_input: f64 = user_input.trim().parse().unwrap();
 
-            let formated_calculation_result_string =
-                format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOperating Expense: {:.2}\n",
-                        income_percentage.tax, income_percentage.owner, income_percentage.profit,
-                        income_percentage.operation_expense);
+                let income_percentage = IncomePercentage {
+                    tax: use_case::calculate_tax_percentage(user_input),
+                    owner: use_case::calculate_owner_percentage(user_input),
+                    profit: use_case::calculate_profit_percentage(user_input),
+                    operation_expense: use_case::calculate_operation_expense(user_input),
+                };
 
-            ui.set_results(formated_calculation_result_string.into());
+                let formated_calculation_result_string =
+                    format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOperating Expense: {:.2}\n",
+                            income_percentage.tax, income_percentage.owner, income_percentage.profit,
+                            income_percentage.operation_expense);
+
+                ui.set_results(formated_calculation_result_string.into());
+            }
         }
     });
 
